22db90ff1449483da301d437ac2703b1dd2453b7
Geronimo Perez <geropereze@gmail.com>
2024-04-10 15:52:25 +1000

    add

---
+++ product-of-array-except-self 10-04-2024.js
@@ -0,0 +1,14 @@
+var productExceptSelf = function(nums) {
+    let prefix = 1;
+    let res = new Array(nums.length).fill(1);
+    for (let i = 0; i < nums.length; i++) {
+        res[i] = prefix;
+        prefix *= nums[i];
+    }
+    let postfix = 1;
+    for (let i = nums.length - 1; i >= 0; i--) {
+        res[i] *= postfix;
+        postfix *= nums[i];
+    }
+    return res;
+};
