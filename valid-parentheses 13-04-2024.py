d329accb4388fcea6e11c15649e42b0096a990ee
Geronimo Perez <geropereze@gmail.com>
2024-04-14 19:08:55 +1000

    idk

--- valid-parentheses 13-04-2024.py
+++ valid-parentheses 13-04-2024.py
@@ -1,12 +1,12 @@
 def isValid(s):
-    dictionary = {')': '(', '}': '{', ']': '['}
-    stack = []
-    for c in s:
-        if c in dictionary:
-            if stack and stack[-1] == dictionary[c]:
-                stack.pop()
+        dictionary = {')': '(', '}': '{', ']': '['}
+        stack = []
+        for c in s:
+            if c in dictionary:
+                if stack and stack[-1] == dictionary[c]:
+                    stack.pop()
+                else:
+                    return False
             else:
-                return False
-        else:
-            stack.append(c)
-    return True if not stack else False
+                stack.append(c)
+        return True if not stack else False
